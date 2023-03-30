6d53fa4ea83a2ce2d278fa76aa59a14d54334ac8
legonzaur <legonzaur@live.fr>
2023-03-30 14:21:50 +0200

    change repository name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/Tone_WebClient/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/ToneAPI_webclient/' : '/'
 })
