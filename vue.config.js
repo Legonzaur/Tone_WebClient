1eef943a0f9f9ab54fe2057738f668f722f4b102
legonzaur <legonzaur@live.fr>
2023-05-05 21:51:02 +0200

    'publish'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/ToneAPI_webclient/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/ToneAPI_webclient/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Players : Tone API'
+    },
+    server: {
+      entry: 'src/main.ts',
+      template: 'public/servers.html',
+      filename: 'servers.html',
+      title: 'Servers : Tone API'
+    }
+  }
 })
