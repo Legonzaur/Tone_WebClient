9f596b9f5041a9f7a997671517a4800fa8635831
legonzaur <legonzaur@live.fr>
2023-06-01 10:36:49 +0200

    'publish'

--- publish.cmd
+++
@@ -1,4 +0,0 @@
-call npm run build
-call git add dist -f
-call git commit -m 'publish'
-call git subtree push --prefix dist origin gh-pages
--- src/@types/reactivity.d.ts
+++
@@ -1,7 +0,0 @@
-import { Ref } from '@vue/reactivity'
-
-declare module '@vue/reactivity' {
-  export interface Ref<T> extends Ref<T> {
-    _rawValue: T
-  }
-}
